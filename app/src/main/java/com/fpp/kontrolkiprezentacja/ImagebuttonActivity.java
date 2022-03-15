d0ac822e72e55d9f68b0d9aae707bb7c449a061f
Filippo0420 <fpawelec0@gmail.com>
2022-03-15 16:02:13 +0100

    button spinner seekbar imagebutton

--- app/src/main/java/com/fpp/kontrolkiprezentacja/ImagebuttonActivity.java
+++ app/src/main/java/com/fpp/kontrolkiprezentacja/ImagebuttonActivity.java
@@ -2,13 +2,27 @@ package com.fpp.kontrolkiprezentacja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ImagebuttonActivity extends AppCompatActivity {
+
+    private ImageButton imbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagebutton);
+
+        imbutton = findViewById(R.id.klikObraz);
+        imbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ImagebuttonActivity.this, cosActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
